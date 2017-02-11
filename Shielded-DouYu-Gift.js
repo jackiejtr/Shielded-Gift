2e8f00e3d209bbd7300c892a974feda7d41e24b7
LisonFan <lisonfan1996@gmail.com>
2017-02-11 15:30:41 +0800

    2017-02-11 15:30:38

--- Shielded-DouYu-Gift.js
+++ Shielded-DouYu-Gift.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @icon         https://www.douyu.com/favicon.ico
 // @name         屏蔽斗鱼礼物特效
-// @version      0.7
+// @version      0.7.1
 // @description  自动屏蔽斗鱼礼物特效
 // @author       LisonFan
-// @match        https://*.douyu.com/*
+// @match        *://*.douyu.com/*
 // @grant        none
 // @namespace	 https://lisonfan.com
 // ==/UserScript==
