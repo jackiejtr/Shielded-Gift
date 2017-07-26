1e385f680ea7f539f8dc5bbf95446060558191ce
LisonFan <lisonfan1996@gmail.com>
2017-07-26 14:19:22 +0800

    增加熊猫直播的屏蔽支持

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Shielded-DouYu-Gift
-屏蔽斗鱼礼物特效
+# 介绍
+自动屏蔽直播礼物特效，目前支持：斗鱼、熊猫直播
+
+# 推荐
+配合 [Stylish](https://userstyles.org/) 扩展使用效果更好。
+
+样式：
+1、[Douyu Cleaner 清爽斗鱼](https://userstyles.org/styles/132037/douyu-cleaner)
+2、[Panda TV Clean|熊猫TV清净](http://userstyles.org/styles/129393)
+3、[斗鱼页面净化](https://userstyles.org/styles/133999/theme)
--- Shielded-DouYu-Gift.js
+++ Shielded-DouYu-Gift.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @icon         https://www.douyu.com/favicon.ico
-// @name         屏蔽斗鱼礼物特效
-// @version      0.8
-// @description  自动屏蔽斗鱼礼物特效
+// @name         屏蔽直播礼物特效
+// @version      1.0
+// @description  自动屏蔽斗鱼、熊猫礼物特效
 // @author       LisonFan
 // @match        *://*.douyu.com/*
+// @match        *://*.panda.tv/*
 // @grant        none
 // @namespace	 https://lisonfan.com
 // ==/UserScript==
@@ -12,9 +12,18 @@
 (function(){
     'use strict';
     document.onreadystatechange = subSomething;
+    var hostName = document.domain;
+    var pandaReg = /(panda.tv)/;
+    var douyuReg = /(douyu.com)/;
     function subSomething() {
         if (document.readyState == 'complete'){
-            document.getElementById("shie-switch").click();
+            if (douyuReg.test(hostName)) {
+                document.getElementById("shie-switch").click(); // 屏蔽所有礼物特效
+            } else if (pandaReg.test(hostName)) {
+                document.getElementById("gift-forbid-option-forbid_chat_gift").click(); // 屏蔽聊天框横幅
+                document.getElementById("gift-forbid-option-forbid_flash_gift").click(); // 屏蔽播放器礼物滚动
+                document.getElementById("gift-forbid-option-forbid_chat_notice").click(); // 屏蔽聊天框消息通知
+            }
         }
     }
 })();
