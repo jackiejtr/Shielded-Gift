900775d1e9b88540bb11234c7f12d666beee4fcf
LisonFan <lisonfan1996@gmail.com>
2017-07-26 14:39:21 +0800

    增加战旗的礼物特效屏蔽支持

--- Shielded-Gift.js
+++ Shielded-Gift.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         屏蔽直播礼物特效
-// @version      1.0
-// @description  自动屏蔽斗鱼、熊猫礼物特效
+// @version      1.0.1
+// @description  自动屏蔽斗鱼、熊猫、战旗礼物特效
 // @author       LisonFan
 // @match        *://*.douyu.com/*
 // @match        *://*.panda.tv/*
+// @match        *://*.zhanqi.tv/*
 // @grant        none
 // @namespace	 https://lisonfan.com
 // ==/UserScript==
@@ -15,6 +16,7 @@
     var hostName = document.domain;
     var pandaReg = /(panda.tv)/;
     var douyuReg = /(douyu.com)/;
+    var zhanqiReg = /(zhanqi.tv)/;
     function subSomething() {
         if (document.readyState == 'complete'){
             if (douyuReg.test(hostName)) {
@@ -23,6 +25,8 @@
                 document.getElementById("gift-forbid-option-forbid_chat_gift").click(); // 屏蔽聊天框横幅
                 document.getElementById("gift-forbid-option-forbid_flash_gift").click(); // 屏蔽播放器礼物滚动
                 document.getElementById("gift-forbid-option-forbid_chat_notice").click(); // 屏蔽聊天框消息通知
+            } else if (zhanqiReg.test(hostName)) {
+                document.getElementById("js-gift-shield").click(); // 屏蔽小礼物的显示
             }
         }
     }
